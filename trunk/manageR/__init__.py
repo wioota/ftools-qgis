ab5d269ccf9d67cc1ed545957a151c3c2ec156bb
carson.farmer <carson.farmer@da625efa-de6f-11dd-a738-5fe2e3cfbcc5>
2009-03-03 01:51:19 +0000

    initial input of manageR (rpy2 version) and voronoipolygons (full python implementation)

---
+++ trunk/manageR/__init__.py
@@ -0,0 +1,50 @@
+#-----------------------------------------------------------
+# 
+# manageR
+#
+# A QGIS plugin for loosely coupling QGIS with the R statistical
+# programming language. Allows uplaod of QGIS layers directly
+# into R, and the ability to perform R operations on the data
+# directly from within QGIS.
+#
+# Copyright (C) 2008  Carson Farmer
+#
+# EMAIL: carson_DOT_farmer_AT_gmail_DOT_com
+# WEB  : www.geog.uvic.ca/spar/carson
+#
+#-----------------------------------------------------------
+# 
+# licensed under the terms of GNU GPL 2
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+# 
+#---------------------------------------------------------------------
+
+from manageR import manageRPlugin
+
+def name():
+  return "manageR"
+
+def description():
+  return "Interface to the R statistical analysis program"
+
+def version():
+  return "Version 0.5"
+  
+def qgisMinimumVersion():
+  return "1.0.0"
+
+def classFactory(iface):
+  return manageRPlugin(iface)
---
+++ trunk/voronoipolygons/doVoronoiPolygons.py
@@ -0,0 +1,169 @@
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
+from qgis.core import *
+from qgis.gui import *
+import ftools_utils
+from frmVoronoi import Ui_Dialog
+import voronoi
+
+class Dialog( QDialog, Ui_Dialog ):
+	def __init__( self, iface ):
+		QDialog.__init__( self )
+		self.iface = iface
+		# Set up the user interface from Designer.
+		self.setupUi( self )
+		self.polygons = False
+		QObject.connect (self.outTool, SIGNAL( "clicked()" ), self.outFile )
+
+		layer_list = ftools_utils.getLayerNames( [ QGis.Point, QGis.Line, QGis.Polygon ] )
+		self.inShape.addItems( layer_list )
+
+	def accept(self):
+		if self.inShape.currentText() == "":
+			QMessageBox.information(self, "Voronoi/Thiessen tesselation", "Please specify input point layer")
+		elif self.outShape.text() == "":
+			QMessageBox.information(self, "Voronoi/Thiessen tesselations", "Please specify output polygon shapefile")
+		else:
+			self.outShape.clear()
+			self.vlayer = ftools_utils.getVectorLayerByName( self.inShape.currentText() )
+			self.voronoi_polygons()
+
+
+	def outFile(self):
+		self.outShape.clear()
+		(self.shapefileName, self.encoding) = ftools_utils.saveDialog(self)
+		if self.shapefileName is None or self.encoding is None:
+			return
+		self.outShape.setText(QString(self.shapefileName))
+
+	def voronoi_polygons( self ):
+		vprovider = self.vlayer.dataProvider()
+		allAttrs = vprovider.attributeIndexes()
+		vprovider.select( allAttrs )
+		fields = vprovider.fields()
+		if self.polygons:
+			geom_type = QGis.WKBPolygon
+		else:
+			geom_type = QGis.WKBLineString
+		writer = QgsVectorFileWriter( self.shapefileName, self.encoding, 
+		fields, geom_type, vprovider.crs() )
+		inFeat = QgsFeature()
+		points = []
+		
+		while vprovider.nextFeature( inFeat ):
+			inGeom = QgsGeometry( inFeat.geometry() )
+			point = inGeom.asPoint()
+			points.append( point )
+		bounds = self.vlayer.extent()
+		vprovider.rewind()
+		vprovider.select( allAttrs )
+		if False:
+			self.delaunay_triangulation( writer, vprovider, points )
+		else:
+			self.voronoi_tesselation( writer, vprovider, points, bounds )
+
+
+	def voronoi_tesselation( self, writer, provider, points, bounds ):
+		coords, equation, edges = voronoi.computeVoronoiDiagram( points )
+		all_attrs = provider.attributeIndexes()
+		provider.select( all_attrs )
+		feat = QgsFeature()
+		print coords
+		print equation
+		print edges
+		for index,v1,v2 in edges:
+			a,b,c = equation[ index ]
+			x_0,y_0 = coords[ v1 ]
+			x_1,y_1 = coords[ v2 ]
+			line = [ QgsPoint( x_0, y_0 ), QgsPoint( x_1, y_1 ) ]
+			geometry = QgsGeometry().fromPolyline( line )
+			feat.setGeometry( geometry )
+			writer.addFeature( feat )
+			if index == 3:
+				break
+		rect = [ 
+		QgsPoint( bounds.xMinimum(), bounds.yMinimum() ),
+		QgsPoint( bounds.xMaximum(), bounds.yMinimum() ),
+		QgsPoint( bounds.xMaximum(), bounds.yMaximum() ),
+		QgsPoint( bounds.xMinimum(), bounds.yMaximum() ),
+		QgsPoint( bounds.xMinimum(), bounds.yMinimum() ) ]
+		geometry = QgsGeometry().fromPolyline( rect )
+		feat.setGeometry( geometry )
+		writer.addFeature( feat )
+		del writer
+			
+		
+	def delaunay_triangulation( self, writer, provider, points ):
+		triangles = voronoi.computeDelaunayTriangulation( points )
+		all_attrs = provider.attributeIndexes()
+		provider.select( all_attrs )
+		feat = QgsFeature()
+		for triangle in triangles:
+			indicies = list( triangle )
+			indicies.append( indicies[ 0 ] )
+			polygon = []
+			for index in indicies:
+				provider.featureAtId( index, feat, True,  all_attrs)
+				geom = QgsGeometry( feat.geometry() )
+				point = QgsPoint( geom.asPoint() )
+				polygon.append( point )
+			geometry = QgsGeometry().fromPolygon( [ polygon ] )
+			feat.setGeometry( geometry )
+			writer.addFeature( feat )
+		del writer
+
+	def out_codes( x, y ):
+		code = 0; 
+		if y > bounds.yMaximum():
+			code += 1 # code for above
+		elif y < bounds.yMinimum():
+			code += 2 # code for below
+		if x > bounds.xMaximum():
+			code += 4 # code for right
+		elif x < bounds.xMinimum():
+			code += 8 # code for left
+		return Code
+		
+	def reject_check( out_code1, out_code2 ):
+		if not out_code1 == 0 and not out_code2 == 0:
+			return True
+		return False
+
+	def accept_check( out_code1, out_code2 ):
+		if out_code1 == 0 and out_code2 == 0:
+			return True
+		return False
+
+	def cohen_sutherland_clip( point1, point2, bounds ):
+		while True:
+			outCode0 = self.out_codes( point1 )
+			outCode1 = self.out_codes( point2 )
+			if self.reject_check( outCode0, outCode1 ):
+				return False
+			if self.accept_check( outCode0, outCode1 ):
+				return True
+			if outCode0 == 0:
+				tempCoord = point0[ 0 ]
+				point0[ 0 ] = point1[ 0 ]
+				point1[ 0 ] = tempCoord
+				tempCoord = point0[ 1 ]
+				point0[ 1 ]= point1[ 1 ]
+				point1[ 1 ] = tempCoord
+				tempCode = outCode0
+				outCode0 = outCode1
+				outCode1 = tempCode
+			if not ( outCode0 & 1 ) == 0:
+				point0[ 0 ] += ( point0[ 1 ] - point0[ 0 ] ) * ( bounds.yMaximum() - point0[ 1 ] ) / ( point1[ 1 ] - point0[ 1 ] )
+				point0[ 1 ] = bounds.yMaximum()
+			elif not ( outCode0 & 2 ) == 0:
+				point0[ 0 ] += ( point1[ 0 ] - point0[ 0 ] ) * ( bounds.yMinimum() - point0[ 1 ] ) / ( point1[ 1 ] - point0[ 1 ] )
+				point0[ 1 ] = bounds.yMinimym()
+			elif not ( outCode0 & 4 ) == 0:
+				point0[ 1 ] += ( point1[ 1 ] - point0[ 1 ] ) * ( bounds.xMaximum() - point0[ 0 ] ) / ( point1[ 0 ] - point0[ 0 ] )
+				point0[ 0 ] = bounds.xMaximum()
+			elif not ( outCode0 & 8 ) == 0:
+				point0[ 1 ] += ( point1[ 1 ] - point0[ 1 ] ) * ( bounds.xMinimum() - point0[ 0 ] ) / ( point1[ 0 ] - point0[ 0 ] )
+				point0[ 0 ] = bounds.xMinimum()
+
+

8cb281353eb345a631cd4ffff9346418463339ea
carson.farmer <carson.farmer@da625efa-de6f-11dd-a738-5fe2e3cfbcc5>
2009-03-09 00:27:10 +0000

    nearly create proper voronoi polygons - changes to voronoi.py are minor changes to doVoronoiPolygons.py are significant -> directional problems in some cases remain

--- voronoipolygons/doVoronoiPolygons.py
+++ voronoipolygons/doVoronoiPolygons.py
@@ -55,39 +55,86 @@ class Dialog( QDialog, Ui_Dialog ):
 			inGeom = QgsGeometry( inFeat.geometry() )
 			point = inGeom.asPoint()
 			points.append( point )
-		bounds = self.vlayer.extent()
 		vprovider.rewind()
 		vprovider.select( allAttrs )
 		if False:
 			self.delaunay_triangulation( writer, vprovider, points )
 		else:
-			self.voronoi_tesselation( writer, vprovider, points, bounds )
+			self.voronoi_tesselation( writer, vprovider, points )
 
 
-	def voronoi_tesselation( self, writer, provider, points, bounds ):
-		coords, equation, edges = voronoi.computeVoronoiDiagram( points )
+	def voronoi_tesselation( self, writer, provider, points ):
+		coords, equation, edges, bounds = voronoi.computeVoronoiDiagram( points )
 		all_attrs = provider.attributeIndexes()
 		provider.select( all_attrs )
 		feat = QgsFeature()
-		print coords
-		print equation
-		print edges
+		count = 0
 		for index,v1,v2 in edges:
 			a,b,c = equation[ index ]
 			x_0,y_0 = coords[ v1 ]
 			x_1,y_1 = coords[ v2 ]
+			if v1 < 0:
+				if a == 1.0:
+					if b < 0:
+						y = bounds[1]
+					elif b > 0:
+						y = bounds[3]
+					x = c - b * y
+				else: # b ==1
+					if a < 0:
+						y = bounds[1]
+					elif a > 0:
+						y = bounds[3]
+					x = ( c - y ) / a
+				if x < bounds[0] or x > bounds[2]:
+					if x < bounds[0]:
+						x = bounds[0]
+					elif x > bounds[2]:
+						x = bounds[2]
+					if a == 1:
+						y = ( c - x ) / b
+					else: # b == 1
+						y = c - a * x
+				x_0 = x
+				y_0 = y
+			elif v2 < 0:
+				if a == 1.0:
+					y = bounds[1]
+					x = ( c - b * y )
+					if x < bounds[0] or x > bounds[2]:
+						if x < bounds[0]:
+							x = bounds[0]
+						elif x > bounds[2]:
+							x = bounds[2]
+						y = ( c - x ) / b
+				elif b == 1.0:
+					if a < 0:
+						x = bounds[2]
+					elif a > 0:
+						x = bounds[0]
+					y = ( c - a * x )
+					if y < bounds[1] or y > bounds[3]:
+						if y < bounds[1]:
+							y = bounds[1]
+						elif y > bounds[3]:
+							y = bounds[3]
+						x = ( c - y ) / a
+				x_1 = x
+				y_1 = y
+			else:
+				x_0,y_0 = coords[ v1 ]
+				x_1,y_1 = coords[ v2 ]
 			line = [ QgsPoint( x_0, y_0 ), QgsPoint( x_1, y_1 ) ]
 			geometry = QgsGeometry().fromPolyline( line )
+#			feat.addAttribute(0,QVariant(test))
 			feat.setGeometry( geometry )
 			writer.addFeature( feat )
-			if index == 3:
-				break
 		rect = [ 
-		QgsPoint( bounds.xMinimum(), bounds.yMinimum() ),
-		QgsPoint( bounds.xMaximum(), bounds.yMinimum() ),
-		QgsPoint( bounds.xMaximum(), bounds.yMaximum() ),
-		QgsPoint( bounds.xMinimum(), bounds.yMaximum() ),
-		QgsPoint( bounds.xMinimum(), bounds.yMinimum() ) ]
+		QgsPoint( bounds[0],bounds[1] ),
+		QgsPoint( bounds[2], bounds[1] ),
+		QgsPoint( bounds[2], bounds[3] ),
+		QgsPoint( bounds[0], bounds[3] ),
+		QgsPoint( bounds[0],bounds[1] ) ]
 		geometry = QgsGeometry().fromPolyline( rect )
 		feat.setGeometry( geometry )
 		writer.addFeature( feat )

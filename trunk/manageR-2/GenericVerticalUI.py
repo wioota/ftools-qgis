ab9ce2bd6b1141953fd095bbde0c763585e9d268
carson.farmer <carson.farmer@da625efa-de6f-11dd-a738-5fe2e3cfbcc5>
2009-09-16 23:47:23 +0000

    Graphic widget now supports popup menu; added voronoi polygons and centroid functions to tools.xml (currently has half implementation of generate weights function); Added license file; Added copyright info to all files; Added plugin icon; Change about dialog.

--- trunk/manageR-2/GenericVerticalUI.py
+++ trunk/manageR-2/GenericVerticalUI.py
@@ -1,4 +1,37 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+'''
+This file is part of manageR
+
+Copyright (C) 2008-9 Carson J. Q. Farmer
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public Licence as published by the Free Software
+Foundation; either version 2 of the Licence, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.  See the GNU General Public Licence for more 
+details.
+
+You should have received a copy of the GNU General Public Licence along with
+this program (see LICENSE file in install directory); if not, write to the Free 
+Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+
+Portions of the console and EditR window, as well as several background 
+funtions are based on the Sandbox python gui of Mark Summerfield.
+  Copyright (C) 2007-9 Mark Summerfield. All rights reserved.
+  Released under the terms of the GNU General Public License.
+The plugins functinality is based largely on the PostGisTools plugin of Mauricio de Paulo.
+  Copyright (C) 2009 Mauricio de Paulo. All rights reserved.
+  Released under the terms of the GNU General Public License.
+manageR makes extensive use of rpy2 (Laurent Gautier) to communicate with R.
+  Copyright (C) 2008-9 Laurent Gautier.
+  Rpy2 may be used under the terms of the GNU General Public License.
+'''
+
 from PyQt4 import QtCore, QtGui
 import rpy2.robjects as robjects
 
--- trunk/manageR-2/pluginManager.py
+++ trunk/manageR-2/pluginManager.py
@@ -1,10 +1,43 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+'''
+This file is part of manageR
+
+Copyright (C) 2008-9 Carson J. Q. Farmer
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public Licence as published by the Free Software
+Foundation; either version 2 of the Licence, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.  See the GNU General Public Licence for more 
+details.
+
+You should have received a copy of the GNU General Public Licence along with
+this program (see LICENSE file in install directory); if not, write to the Free 
+Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+
+Portions of the console and EditR window, as well as several background 
+funtions are based on the Sandbox python gui of Mark Summerfield.
+  Copyright (C) 2007-9 Mark Summerfield. All rights reserved.
+  Released under the terms of the GNU General Public License.
+The plugins functinality is based largely on the PostGisTools plugin of Mauricio de Paulo.
+  Copyright (C) 2009 Mauricio de Paulo. All rights reserved.
+  Released under the terms of the GNU General Public License.
+manageR makes extensive use of rpy2 (Laurent Gautier) to communicate with R.
+  Copyright (C) 2008-9 Laurent Gautier.
+  Rpy2 may be used under the terms of the GNU General Public License.
+'''
+
 # Import the PyQt and QGIS libraries
 from PyQt4.QtCore import * 
 from PyQt4.QtGui import *
-from PyQt4.QtSql import *
+#from PyQt4.QtSql import *
 from qgis.core import *
-import os
+import os, resources
 from xml.dom import minidom
 
 from GenericVerticalUI import GenericVerticalUI
@@ -13,7 +46,7 @@ class PluginManager:
     def __init__(self, parent):#, iface):
         ## Save reference to the QGIS interface
         #self.iface = iface
-        self.tools="/home/cfarmer/.qgis/python/plugins/manageR/tools.xml"
+        self.tools= os.path.join(os.path.dirname( __file__ ),"tools.xml")
         self.parent = parent
 
     def makeCaller(self, n):
@@ -36,7 +69,7 @@ class PluginManager:
                     name= tool.getAttribute("name")
                     # Create action that will start plugin configuration
                     self.actionlist.append(QAction(
-                    QIcon("mActionPluginAction"), name, self.parent))
+                    QIcon(":mActionPluginsPlugin"), name, self.parent))
                     #create a new funcion that calls run() with the id parameter
                     self.callerlist.append(self.makeCaller(len(self.actionlist)-1)) 
                     # connect the action to the run method
